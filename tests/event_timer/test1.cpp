99ee7f4ef8cc2b18ae05799215c6316d99e15900
IQuesadilla <shologuckers.iq@gmail.com>
2023-09-09 10:19:04 -0600

    Updated event_timer_test1 to test multiple timers

--- tests/event_timer/test1.cpp
+++ tests/event_timer/test1.cpp
@@ -5,28 +5,43 @@
 class my_etimer_child : public libQ::etimer_child
 {
 public:
+    my_etimer_child(int msdelay) : libQ::etimer_child()
+    {
+        _msdelay = std::chrono::milliseconds(msdelay);
+        _start = std::chrono::system_clock::now();
+        next_event = _start + _msdelay;
+    }
+
     void etimer_update()
     {
-        std::cout << "function: etimer_update" << std::endl;
         myUpdate();
     }
 
 private:
     void myUpdate()
     {
-        std::cout << std::chrono::system_clock::now().time_since_epoch().count() << " : myUpdate" << std::endl;
-        next_event = std::chrono::system_clock::now() + std::chrono::milliseconds(1000);
+        std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - _start).count() << " : " << _msdelay.count() << " : myUpdate" << std::endl;
+        next_event += _msdelay;
     }
+
+    std::chrono::milliseconds _msdelay;
+    std::chrono::system_clock::time_point _start;
 };
 
 int main()
 {
     libQ::event_timer timer;
 
-    my_etimer_child object;
-    timer.registerNewChild(&object);
+    my_etimer_child object1(1000);
+    timer.registerNewChild(&object1);
 
-    for (int i = 0; i < 5; ++i)
+    my_etimer_child object2(300);
+    timer.registerNewChild(&object2);
+
+    my_etimer_child object3(1300);
+    timer.registerNewChild(&object3);
+
+    for (int i = 0; i < 15; ++i)
     {
         timer.update_children();
     }
