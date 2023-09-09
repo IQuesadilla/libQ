1b46f280c9e5efa6d59cb2c9046df5750266910f
IQuesadilla <shologuckers.iq@gmail.com>
2023-09-09 13:49:43 -0600

    After much wasted time, event_timer now works using templates

--- include/event_timer.h
+++ include/event_timer.h
@@ -6,6 +6,8 @@
 #include <chrono>
 #include <vector>
 #include <cstdint>
+#include <memory>
+#include <functional>
 
 // Creates a vector of etimer_child objects and 
 
@@ -14,24 +16,37 @@ namespace libQ {
 class etimer_child
 {
 public:
+	virtual ~etimer_child(){};
 	virtual void etimer_update() = 0;
 	std::chrono::system_clock::time_point next_event;
 };
 
+template <typename T>
 class event_timer
 {
 public:
-	event_timer();
+	event_timer(std::shared_ptr<std::vector<T> > ptr)
+	{
+		_ptr = ptr;
+	}
 
-	void registerNewChild(etimer_child *child);
+	void update_children()
+	{
+		T soonest = _ptr->at(0);
+		for (auto &child : *_ptr)
+		{
+			if ( child->next_event < soonest->next_event )
+			{
+				soonest = child;
+			}
+		}
 
-	void update_children();
+		std::this_thread::sleep_until(soonest->next_event);
+		soonest->etimer_update();
+	}
 
 private:
-	std::vector<etimer_child*> _timers;
-
-	std::chrono::system_clock::time_point _start;
-	std::chrono::system_clock::time_point _currentCycleTime;
+	std::shared_ptr<std::vector<T> > _ptr;
 };
 
 };
--- tests/event_timer/test1.cpp
+++ tests/event_timer/test1.cpp
@@ -30,19 +30,47 @@ private:
 
 int main()
 {
-    libQ::event_timer timer;
-
-    my_etimer_child object1(1000);
-    timer.registerNewChild(&object1);
-
-    my_etimer_child object2(300);
-    timer.registerNewChild(&object2);
-
-    my_etimer_child object3(1300);
-    timer.registerNewChild(&object3);
-
-    for (int i = 0; i < 15; ++i)
     {
-        timer.update_children();
+        std::vector<my_etimer_child*> objarray;
+        std::shared_ptr<std::vector<my_etimer_child*>> objects(&objarray,[](void*){});
+
+        my_etimer_child object1(1000);
+        objects->push_back(&object1);
+        //timer.push_back(new my_etimer_child(&object1));
+
+        my_etimer_child object2(300);
+        objects->push_back(&object2);
+        //timer.push_back(std::shared_ptr<my_etimer_child>(&object2,[](void*){}));
+
+        my_etimer_child object3(1300);
+        objects->push_back(&object3);
+        //timer.push_back(std::shared_ptr<my_etimer_child>(&object3,[](void*){}));
+
+        libQ::event_timer<my_etimer_child*> timer(objects);
+
+        for (int i = 0; i < 15; ++i)
+        {
+            timer.update_children();
+        }
+    }
+
+    {
+        std::shared_ptr<std::vector<my_etimer_child*>> objects;
+        objects.reset(new std::vector<my_etimer_child*>);
+        libQ::event_timer<my_etimer_child*> timer(objects);
+    
+        objects->push_back(new my_etimer_child(1000));
+        objects->push_back(new my_etimer_child(300));
+        objects->push_back(new my_etimer_child(1300));
+
+        for (int i = 0; i < 15; ++i)
+        {
+            timer.update_children();
+        }
+
+        for (auto &x : *objects)
+        {
+            delete x;
+        }
     }
 }
